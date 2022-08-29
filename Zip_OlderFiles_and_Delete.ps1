8038beeafa31b8f6fef18b842210cb72342033b7
souravkarmohapatra <105043552+souravkarmohapatra@users.noreply.github.com>
2022-08-29 09:52:15 +0200

    Add files via upload

---
+++ Zip_OlderFiles_and_Delete.ps1
@@ -0,0 +1,6 @@
+﻿cd C:\diskspacetest
+$Files = Get-ChildItem | Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-2)}
+ForEach ($File in $Files) {
+    $File | Compress-Archive -DestinationPath "C:\zipfolder\($File.archived).zip"
+    Remove-Item $File
+}
